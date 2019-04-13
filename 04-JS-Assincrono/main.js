d58b4d9049aacdef4fa770a0fd94edd34445fbfe
José Malcher Jr <contato@josemalcher.net>
2019-04-13 17:46:01 -0300

    24 - Promises

--- 04-JS-Assincrono/main.js
+++ 04-JS-Assincrono/main.js
@@ -1,10 +1,26 @@
-var xhr = new XMLHttpRequest();
+var minhaPrimise = function(){
+    return new Promise(function(resolve,reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://apii.github.com/users/josemalcher');
+        xhr.send(null);
 
-xhr.open('GET', 'https://api.github.com/users/josemalcher');
-xhr.send(null);
-
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            }else{
+                reject('ERROR NA REQUISIÇÃO');
+            }
+        }
+    }   
+    });
 }
+
+minhaPrimise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error)
+    });
+
--- README.md
+++ README.md
@@ -922,7 +922,35 @@ xhr.onreadystatechange = function(){
 
 ## <a name="parte24">24 - Promises</a>
 
+```javascript
+var minhaPrimise = function(){
+    return new Promise(function(resolve,reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://apii.github.com/users/josemalcher');
+        xhr.send(null);
 
+        xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                resolve(JSON.parse(xhr.responseText));
+            }else{
+                reject('ERROR NA REQUISIÇÃO');
+            }
+        }
+    }   
+    });
+}
+
+minhaPrimise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error)
+    });
+
+
+```
 
 [Voltar ao Índice](#indice)
 
